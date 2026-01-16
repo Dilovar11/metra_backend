1a6d711c929cfbc7c68dd3dd8da5a998c030878e
Makhkamov <dilovarmahkamov6@gmail.com>
2026-01-17 02:15:52 +0500

    vercel

--- api/main.ts
+++ api/main.ts
@@ -14,15 +14,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
   app.enableCors();
 
-  const config = new DocumentBuilder()
-    .setTitle('NanoBanana API')
-    .setDescription('API NanoBanana')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
   await app.init(); // важный шаг для serverless
 }
