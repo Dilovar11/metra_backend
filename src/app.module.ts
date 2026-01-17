f5304403560aeaf77db46066397c7b6142419697
Makhkamov <dilovarmahkamov6@gmail.com>
2026-01-17 08:55:02 +0500

    vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as dotenv from 'dotenv';
+
 import { NanoBananaModule } from './Modules/nanobanana/nanobanana.module';
-
-
-import { Avatar } from './Entities/avatar.entity';
-import { BodyPhoto } from './Entities/body-photo.entity';
-import { GenerationMedia } from './Entities/generation-media.entity';
-import { Generation } from './Entities/generation.entity';
-import { Purchase } from './Entities/purchase.entity';
-import { Referral } from './Entities/referral.entity';
-import { Subscription } from './Entities/subscription.entity';
-import { SupportTicket } from './Entities/support-ticket.entity';
-import { TokenBalance } from './Entities/token-balance.entity';
-import { TokenTransaction } from './Entities/token-transaction.entity';
-import { User } from './Entities/user.entity';
 import { UsersModule } from './Modules/user/users.module';
 import { AvatarModule } from './Modules/avatar/avatar.module';
 import { BodyPhotoModule } from './Modules/body-photo/body-photo.module';
@@ -27,10 +14,29 @@ import { SupportTicketModule } from './Modules/support-ticket/support-ticket.mod
 import { TokenBalanceModule } from './Modules/token-balance/token-balance.module';
 import { TokenTransactionModule } from './Modules/token-transaction/token-transaction.module';
 
-dotenv.config();
-
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+
+      ssl: {
+        rejectUnauthorized: false,
+      },
+
+      autoLoadEntities: true,
+      synchronize: false,
+      logging: false,
+
+      extra: {
+        max: 5, // важно для Neon
+      },
+    }),
+
     NanoBananaModule,
     UsersModule,
     AvatarModule,
@@ -43,34 +49,6 @@ dotenv.config();
     SupportTicketModule,
     TokenBalanceModule,
     TokenTransactionModule,
-    
-    TypeOrmModule.forRoot({
-           type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: false,
-      ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
-
-      synchronize: false,
-      logging: false,
-
-      entities: [
-        Avatar,
-        BodyPhoto,
-        GenerationMedia,
-        Generation,
-        Purchase,
-        Referral,
-        Subscription,
-        SupportTicket,
-        TokenBalance,
-        TokenTransaction,
-        User
-      ],
-    }),
   ],
 })
 export class AppModule {}
