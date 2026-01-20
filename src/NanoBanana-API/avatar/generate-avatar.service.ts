e141b205b5448334d5ef6e9118c9127897fd5ca3
Makhkamov <dilovarmahkamov6@gmail.com>
2026-01-21 00:21:27 +0500

    fix bag create-avatar

--- src/NanoBanana-API/avatar/generate-avatar.service.ts
+++ src/NanoBanana-API/avatar/generate-avatar.service.ts
@@ -6,7 +6,7 @@ import { GenerateAvatarDto } from './dto/generate-avatar.dto';
 @Injectable()
 export class AvatarGeneratorService {
     private genAI: GoogleGenerativeAI;
-    private readonly IMAGEN_MODEL = 'imagen-3.0-generate-001';
+    private readonly IMAGEN_MODEL = 'gemini-2.5-flash-image';
 
     constructor() {
         const apiKey = process.env.GOOGLE_GEN_AI_KEY
