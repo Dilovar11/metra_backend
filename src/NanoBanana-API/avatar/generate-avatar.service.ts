5f2e063251530a984e4aa3b9857478da0fdeb39a
Makhkamov <dilovarmahkamov6@gmail.com>
2026-01-25 12:11:32 +0500

    upload files avatar to cloudinary

--- src/NanoBanana-API/avatar/generate-avatar.service.ts
+++ src/NanoBanana-API/avatar/generate-avatar.service.ts
@@ -12,7 +12,10 @@ export class AvatarGeneratorService {
 
             if (dto.name && dto.gender && dto.imageFront && dto.imageLeft && dto.imageRight) {
                 
-                const successImageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmgtePIhM5sWc96KUjIIZYYtLkyDcUH13hOA&s';
+                const successImageUrl1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmgtePIhM5sWc96KUjIIZYYtLkyDcUH13hOA&s';
+                const successImageUrl2 = 'https://cdn-icons-png.flaticon.com/512/11122/11122443.png';
+                const successImageUrl3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTymEBwTy4adePMBn4Zln-smukrUFsI8FyDnA&s';
+                const successImageUrl4 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP9Sbu9iUE6UPNtmd9E6L8AZSX2Vo6hmkI4w&s';
 
                 console.log('--- Mock Generation Finished Successfully ---');
                 
@@ -20,10 +23,10 @@ export class AvatarGeneratorService {
                     name: dto.name,
                     gender: dto.gender, 
                     imagesURL: [
-                        successImageUrl,
-                        successImageUrl,
-                        successImageUrl,
-                        successImageUrl
+                        successImageUrl1,
+                        successImageUrl2,
+                        successImageUrl3,
+                        successImageUrl4
                     ]
                 };
             } else {
