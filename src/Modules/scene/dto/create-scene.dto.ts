80afb18f0875b007ff247fc4f5318e70803d5507
Makhkamov <dilovarmahkamov6@gmail.com>
2026-01-29 23:00:51 +0500

    fix bags

--- src/Modules/scene/dto/create-scene.dto.ts
+++ src/Modules/scene/dto/create-scene.dto.ts
@@ -1,13 +1,18 @@
+// create-scene.dto.ts
 import { ApiProperty } from '@nestjs/swagger';
 import { SceneType } from '../../../Entities/scene.entity';
 
 export class CreateSceneDto {
-  @ApiProperty({ enum: SceneType, example: SceneType.HOME_PORTRAIT })
-  type: SceneType;
+    @ApiProperty({
+        enum: SceneType,
+        enumName: 'SceneType', 
+        example: SceneType.HOME_PORTRAIT
+    })
+    type: SceneType;
 
-  @ApiProperty({ example: 'Уютный вечер у камина' })
-  name: string;
+    @ApiProperty({ example: 'Уютный вечер у камина' })
+    name: string;
 
-  @ApiProperty({ example: 'High quality portrait, cinematic lighting, cozy atmosphere...' })
-  prompt: string;
+    @ApiProperty({ example: 'High quality portrait...' })
+    prompt: string;
 }
