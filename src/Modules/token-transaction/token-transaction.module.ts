d5cff039cf0a74eec247ee588ea35d610c272be2
Makhkamov <dilovarmahkamov6@gmail.com>
2026-02-03 14:10:45 +0500

    addBalance

--- src/Modules/token-transaction/token-transaction.module.ts
+++ src/Modules/token-transaction/token-transaction.module.ts
@@ -5,12 +5,16 @@ import { TokenBalance } from '../../Entities/token-balance.entity';
 import { User } from '../../Entities/user.entity';
 import { TokenTransactionService } from './token-transaction.service';
 import { TokenTransactionController } from './token-transaction.controller';
+import { PaymentTransaction } from '../../Entities/payment-transaction';
+import { TokenBalanceModule } from '../token-balance/token-balance.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([TokenTransaction, TokenBalance, User]),
+    TypeOrmModule.forFeature([TokenTransaction, TokenBalance, User, PaymentTransaction]),
+    TokenBalanceModule
   ],
   providers: [TokenTransactionService],
   controllers: [TokenTransactionController],
+  exports: [TokenTransactionService],
 })
 export class TokenTransactionModule {}
