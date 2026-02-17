d6930142351065122978bc1e4e73b7d14a6bd936
Makhkamov <dilovarmahkamov6@gmail.com>
2026-02-17 23:44:54 +0500

    add decorator

--- src/Modules/auth/auth.module.ts
+++ src/Modules/auth/auth.module.ts
@@ -5,10 +5,12 @@ import { AuthController } from './auth.controller';
 import { User } from '../../Entities/user.entity';
 import { Referral } from '../../Entities/referral.entity';
 import { ReferralCode } from '../../Entities/referral_codes';
+import { ReferralModule } from '../referral/referral.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Referral, ReferralCode])
+    TypeOrmModule.forFeature([User, Referral, ReferralCode]),
+    ReferralModule
   ],
   controllers: [AuthController],
   providers: [AuthService],
