1c848ccf2aa878a9adfac0f36f14595f2ae26051
Makhkamov <dilovarmahkamov6@gmail.com>
2026-02-26 16:23:56 +0500

    fic

--- src/Modules/generation/generation.controller.ts
+++ src/Modules/generation/generation.controller.ts
@@ -4,6 +4,7 @@ import { GenerationService } from './generation.service';
 import { CreateGenerationDto } from './dto/create-generation.dto';
 import { GenerationType } from 'src/Entities/generation.entity';
 import { TgUser } from '../../Common/decorators/user.decorator';
+import { Public } from '../../Common/decorators/public.decorator';
 
 
 @ApiTags('Generations')
