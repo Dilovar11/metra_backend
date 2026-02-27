04c0715a28147f058e21ceec3ba5b482ea8bd26f
Makhkamov <dilovarmahkamov6@gmail.com>
2026-02-27 16:46:29 +0500

    Gemini 4

--- src/NanoBanana-API/generation-image/image-generator.module.ts
+++ src/NanoBanana-API/generation-image/image-generator.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ImageGeneratorController } from './image-generator.controller';
 import { ImageGeneratorService } from './image-generator.service';
+import { FilesModule } from '../../Modules/file/file.module';
 
 @Module({
+    imports: [FilesModule],
     controllers: [ImageGeneratorController],
     providers: [ImageGeneratorService]
 })
