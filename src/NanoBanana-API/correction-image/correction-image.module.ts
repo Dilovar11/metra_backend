97a4a2d4912a0fc651f728a11c99c338f289311b
Makhkamov <dilovarmahkamov6@gmail.com>
2026-03-13 00:27:29 +0500

    generation-by-token

--- src/NanoBanana-API/correction-image/correction-image.module.ts
+++ src/NanoBanana-API/correction-image/correction-image.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CorrectionImageController } from './correction-image.controller';
 import { CorrectionImageService } from './correction-image.service';
+import { FilesModule } from 'src/Modules/file/file.module';
+import { TokenBalanceModule } from 'src/Modules/token-balance/token-balance.module';
 
 @Module({
+  imports: [
+    FilesModule,
+    TokenBalanceModule 
+  ],
   controllers: [CorrectionImageController],
   providers: [CorrectionImageService],
   exports: [CorrectionImageService], 
